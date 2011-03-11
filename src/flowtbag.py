e2e0d404049f3ec038a62cdb7579d3bb63a18972
Daniel Arndt <danielarndt@gmail.com>
2011-03-10 20:45:01 -0400

    Messing around and trying scapy out.

--- src/flowtbag.py
+++ src/flowtbag.py
@@ -4,7 +4,17 @@ Created on Mar 10, 2011
 @author: Daniel Arndt
 '''
 
+from scapy.all import *
 
+def callback(pkt):
+    flags=pkt.sprintf("%TCP.flags%")
+    if (pkt.proto == 6):
+        print "TCP: [SPort %s | DPort %s]" % (pkt.sport, pkt.dport)
+    elif (pkt.proto == 17):
+        print "UDP: [SPort %s | DPort %s]" % (pkt.sport, pkt.dport)
 
 if __name__ == '__main__':
-    print "Hello, world!"
+    try:
+        sniff(offline="test.cap", prn=callback, store=0, count = 5)
+    except KeyboardInterrupt:
+        exit(0)
