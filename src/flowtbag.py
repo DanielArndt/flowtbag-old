67bcd9548f6e8ddd141f0ab239f33230cab51d55
Daniel Arndt <danielarndt@gmail.com>
2011-05-18 16:27:54 -0300

    Made flows bidirectional.

--- src/flowtbag.py
+++ src/flowtbag.py
@@ -23,18 +23,24 @@ class Flow:
         '''
         self.id = id
         self.first_packet = pkt
+        # Basic identification criteria
         self.srcip = pkt[IP].src
         self.srcport = pkt.sport
         self.dstip = pkt[IP].dst
         self.dstport = pkt.dport
+        self.proto = pkt.proto
+        #
+        
         self.total_fpackets = 1
         self.total_bpackets = 0
     
     def __repr__(self):
-        return "A flow"
+        return "[%d:(%s,%d,%s,%d,%d)]" % \
+            (self.id, self.srcip, self.srcport,self.dstip,self.dstport,self.proto) 
         
     def __str__(self):
-        return str(self.id)
+        return "[%d:(%s,%d,%s,%d,%d)]" % \
+            (self.id, self.srcip, self.srcport,self.dstip,self.dstport,self.proto) 
         
     def add(self, pkt):
         self.total_fpackets += 1
@@ -81,15 +87,23 @@ class Flowtbag:
             print "Ignoring non-IP packet %d" % (self.count)
             return
         print "Processing packet %d" % (self.count)
+        srcip = pkt[IP].src
+        srcport = pkt.sport
+        dstip = pkt[IP].dst
+        dstport = pkt.dport
+        proto = pkt.proto
+        flow_tuple = (srcip, srcport, dstip, dstport, proto)
+        flow_tuple = self.sortIPs(flow_tuple)
 
         if (pkt.proto == 6):
             # TCP
-            flow_tuple = (pkt[IP].src, pkt.sport, pkt[IP].dst, pkt.dport, pkt.proto)
             if (flow_tuple not in self.active_flows):
                 self.flow_count += 1
                 self.active_flows[flow_tuple] = Flow(pkt, self.flow_count)
+                print "Created flow %d" % (self.flow_count)
             else:
-                print "Adding packet %d to %s" % (self.count, self.active_flows[flow_tuple])
+                print "Adding packet %d to flow %s" % \
+                    (self.count, self.active_flows[flow_tuple])
                 self.active_flows[flow_tuple].add(pkt)
             #print "%s" % (flow_tuple,)
             #print "TCP Flags: %s" % (pkt[TCP].flags)
@@ -97,7 +111,15 @@ class Flowtbag:
             #print "%s" % (flags)
         elif (pkt.proto == 17):
             # UDP
-            print "UDP: [SPort %s | DPort %s]" % (pkt.sport, pkt.dport)
+            print "UDP: [SPort %s | DPort %s]" % \
+                (pkt.sport, pkt.dport)
+    
+    def sortIPs(self, t):
+        if (t[2] < t[0]):
+            new_tuple = (t[2],t[3],t[0],t[1], t[4])
+        else:
+            new_tuple = t
+        return new_tuple
     
 if __name__ == '__main__':
     Flowtbag()
