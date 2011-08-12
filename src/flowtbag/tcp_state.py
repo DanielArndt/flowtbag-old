7eb28fba6bd735cc7efe08d587775b0848f93be3
Daniel Arndt <danielarndt@gmail.com>
2011-08-12 06:31:58 -0300

    Added header to tcp_state file.

--- src/flowtbag/tcp_state.py
+++ src/flowtbag/tcp_state.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+
+'''
+   Copyright 2011 Daniel Arndt
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+   Contributors:
+
+   @author: Daniel Arndt <danielarndt@gmail.com>
+'''
+
 TCP_FIN = 0x01
 TCP_SYN = 0x02
 TCP_RST = 0x04
